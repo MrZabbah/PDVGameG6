2d9b19c8ef202be9cad9fd151030c131011b1f00
AlejandroOrejudo2000 <AlejandroOrejudo2000@users.noreply.github.com>
2020-03-19 19:23:20 +0100

    Menú de inicio

---
+++ 3D TEST/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Patrón Singleton:
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+         DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            SceneManager.LoadScene("Nivel 1");
+        }
+    }
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ 3D TEST/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] panels;
+
+    // Update is called once per frame
+    void Start()
+    {
+        changePanel(0);
+    }
+    public void changePanel(int num)
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            if (i == num)
+                panels[i].SetActive(true);
+            else
+                panels[i].SetActive(false);
+        }
+    }
+
+    public void closeGame()
+    {
+        Application.Quit(0);
+    }
+
+    public void debugging()
+    {
+       
+    }
+}
